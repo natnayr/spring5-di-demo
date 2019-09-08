d388def78c3967c4d0b7324210e7ae6e7e602789
natnayr <ryantanjunming@gmail.com>
2019-09-09 00:02:30 +0800

    fixed assignment

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -8,11 +8,15 @@ import org.springframework.stereotype.Service;
  * Created by jt on 5/24/17.
  */
 @Service
-@Profile("es")
 @Primary
+@Profile("es")
 public class PrimarySpanishGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
+
+    public PrimarySpanishGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
 
     @Override
     public String sayGreeting() {
